dd2fb5d835c34a653614a169c4f8592f102d3758
Nicholas Mwaniki Musee <nichol.musee@gmail.com>
2025-11-28 00:19:03 +0300

    chore: update env and package.json for deployment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log("MongoDB Connected....!");
     } catch (error) {
         console.error("MongoDB error: ", error.message);
